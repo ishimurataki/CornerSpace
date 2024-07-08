46f582a88ffee78f71677d7b8d12f369ac889781
Takanao Ishimura <ishimurataki@gmail.com>
2024-07-08 15:21:44 +0900

    Add canvas create button on studio page

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -4,6 +4,8 @@ import CanvasCardWrapper from "./canvas-card-wrapper";
 import { getCanvasIdsForSignedInUserServer } from "@/backend-lib/actions";
 import { notFound, redirect } from "next/navigation";
 import { Suspense } from "react";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 export default async function App() {
 
@@ -17,7 +19,13 @@ export default async function App() {
     <main className="absolute flex flex-col m-6 md:m-8 lg:m-10">
       <p className="text-2xl">@{username}&apos; Studio</p>
       <p className="text-lg mx-3 md:mx-6 lg:mx-10 mt-2 md:mt-3 lg:mt-5" >Your Gallery</p >
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-3 md:mx-6 lg:mx-10 mt-2 md:mt-3 lg:mt-5 overflow-scroll">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-3 md:mx-6 lg:mx-10 mt-2 md:mt-3 lg:mt-5 overflow-scroll mb-6">
+        <Link className={`bg-gray-200 w-full aspect-square rounded-lg flex justify-center items-center hover:border-4 border-gray-500 
+        text-gray-500 hover:text-gray-700`}
+          href="/create"
+          title="Create new canvas!">
+          <PlusIcon className="w-1/2 h-1/2" />
+        </Link>
         {canvasIds.map((canvasId) => {
           return (
             <Suspense fallback={
