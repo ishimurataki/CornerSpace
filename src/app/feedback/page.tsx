ac6c9234d986b06cd429130ef0b6ac86966cf146
Takanao Ishimura <ishimurataki@gmail.com>
2024-09-08 17:22:07 +0900

    Fix linting errors once again

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -8,7 +8,7 @@ export default async function FeedbackPage() {
         <main className="absolute flex flex-col my-5 md:my-10 mx-10 md:mx-40 gap-2 pb-20">
             <h1 className="text-3xl">Feedback for CornerSpace</h1>
             <hr className="bg-gray-500" />
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfZESd3k3T_ywCua7KrI7V7TR4sSPl5mV-K1tYnmQapCRm8vA/viewform?embedded=true" width="640" height="1182" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfZESd3k3T_ywCua7KrI7V7TR4sSPl5mV-K1tYnmQapCRm8vA/viewform?embedded=true" width="640" height="1182">Loading…</iframe>
         </main>
     );
 }
