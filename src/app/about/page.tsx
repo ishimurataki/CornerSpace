e3bb030043f5d1d40b1f98b22e7066e4a05e1c3f
Takanao Ishimura <ishimurataki@gmail.com>
2024-09-08 17:04:57 +0900

    Fix linting errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default async function AboutPage() {
         <main className="absolute flex flex-col my-5 md:my-10 mx-10 md:mx-40 gap-2 pb-20">
             <h1 className="text-3xl">About CornerSpace</h1>
             <hr className="bg-gray-500" />
-            <p>Hi all! My name is Taki Ishimura and I'm the sole developer of CornerSpace, this online voxel-art platform that I hope you are enjoying.
+            <p>Hi all! My name is Taki Ishimura and I&apos;m the sole developer of CornerSpace, this online voxel-art platform that I hope you are enjoying.
                 My goal with this project is to make voxel art accessible to everyone and anyone, where users can start creating beautiful pieces of 3D cube art with little to no prior knowledge.
                 The user interface should be intuitive and simple while supporting a broad range of actions and full expressability in your work.
                 On top of that, users should be able to connect with one another, sharing and liking pieces of 3D art that they are proud of.
